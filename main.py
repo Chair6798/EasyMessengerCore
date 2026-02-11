6801c90100c3a7b9595b923c5165e99fac926b0d
Chair6798 <oggh.original@yandex.ru>
2026-02-11 13:25:41 +0300

    dev steps

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from flask import Flask
-# python -m flask --app hello run
+import json
+# python -m flask --app main run
 app = Flask(__name__)
+
+accounts = []
+
+def get_account(id):
+    return accounts[id]
+
+def found_account(username):
+    for account in accounts:
+        if account["username"]==username:
+            return accounts.index(account)
+
+@app.route("/register/<username>/<password>/")
+def reg(username,password):
+    accounts.append({"name":username,"password":password})
+    print(accounts)
+    return "succeful"
