f7cdc4681df8891144a6aece0abe4daef1443c81
Chair6798 <oggh.original@yandex.ru>
2026-02-12 20:02:21 +0300

    upgrade, added classes, btw i will remove it and return to python {} schema

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
-import json
+import json.encoder as json
+import hashlib
+import time
 # python -m flask --app main run
 app = Flask(__name__)
 
 accounts = []
+
+enc = json.JSONEncoder()
+
+sessions = {}
 
 def get_account(id):
     return accounts[id]
@@ -14,11 +20,26 @@ def find_account(username):
             return accounts.index(account)
     return -1
 
+def create_session(account_id):
+    if len(accounts)-1<account_id:
+        return "wrong"
+    sesid = (str(time.time())+str(account_id))
+    sessions[sesid] = account_id
+    return sesid
+    
 @app.route("/register/<username>/<password>/")
 def reg(username,password):
     if find_account(username)!=-1:
         print(f"Failed to create new account. Username {username} is already using")
-        return "usernameusing"
+        return enc.encode({"done":False, "reasoncode":1})
     accounts.append({"username":username,"password":password})
     print("Created new account successfully. Username: "+username)
-    return "succes"
+    return enc.encode({"done":True, "reasoncode":0, "session":create_session(len(accounts)-1)})
+
+@app.route("/login/<username>/<password>/")
+def login(username,password):
+    account_id = find_account(username)
+    if account_id==-1:
+        print("Failed to login to "+username+" Password wrong!")
+    
+    
