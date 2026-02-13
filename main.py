2f70935c1f3769dfcb01c2eb7b1e2bb740d1d0f9
Chair6798 <oggh.original@yandex.ru>
2026-02-13 15:13:19 +0300

    removed classes, ended login function
    idk. i'm tired, i gonna take a free time

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask
 import json.encoder as json
 import hashlib
 import time
+import re
+
+e_p = re.compile(r'^[a-zA-Z]+$')
+
 # python -m flask --app main run
 app = Flask(__name__)
 
@@ -10,6 +14,14 @@ accounts = []
 enc = json.JSONEncoder()
 
 sessions = {}
+
+chats = []
+
+def get_default_account():
+    return {"username":"", "password":"", "chats": []}
+
+def check_reg_name(name: str)->bool:
+    return bool(e_p.fullmatch(name))
 
 def get_account(id):
     return accounts[id]
@@ -26,13 +38,24 @@ def create_session(account_id):
     sesid = (str(time.time())+str(account_id))
     sessions[sesid] = account_id
     return sesid
-    
+
+
+def create_chat(name, root):
+    chat = {"name": name, "root": root, "closed":False, "messages": []}
+    chats.append(chat)
+    return chat
+
 @app.route("/register/<username>/<password>/")
 def reg(username,password):
     if find_account(username)!=-1:
         print(f"Failed to create new account. Username {username} is already using")
         return enc.encode({"done":False, "reasoncode":1})
-    accounts.append({"username":username,"password":password})
+    if check_reg_name(username)==False:
+        return enc.encode({"done":False, "reasoncode":2})
+    acc = get_default_account().copy()
+    acc["username"] = username
+    acc["password"] = password
+    accounts.append(acc)
     print("Created new account successfully. Username: "+username)
     return enc.encode({"done":True, "reasoncode":0, "session":create_session(len(accounts)-1)})
 
@@ -41,5 +64,11 @@ def login(username,password):
     account_id = find_account(username)
     if account_id==-1:
         print("Failed to login to "+username+" Password wrong!")
-    
-    
+        return enc.encode({"done":False, "reasoncode":1})
+    account = get_account(account_id)
+    if account["password"]==password:
+        print("Logged in to "+username+" successfully!")
+        return enc.encode({"done":True, "reasoncode":0, "session":create_session(account_id)})
+    else:
+        print("Failed to login to "+username+" Password wrong!")
+        return enc.encode({"done":False, "reasoncode":2})
